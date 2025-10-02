a44f106115222b95996f81915c0063bc35333f9e
Gerald Donkor <donkorgerald8@gmail.com>
2025-10-02 11:55:23 +0000

    ake the rabbit happy

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,16 +1,19 @@
 'use client';
 
-import { useCallback, useRef } from "react";
-
-
+import { useCallback, useEffect, useRef } from "react";
 export function useDebounce(callback: () => void, delay: number) {
-    const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
+    const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+    useEffect(() => {
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+        };
+    }, []);
     return useCallback(() => {
-        if(timeoutRef.current) {
+        if (timeoutRef.current) {
             clearTimeout(timeoutRef.current);
         }
-
         timeoutRef.current = setTimeout(callback, delay);
-    }, [callback, delay])
+    }, [callback, delay]);
 }
