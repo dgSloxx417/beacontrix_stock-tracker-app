dedbdea9d7cd37570f0ac52b729ca8c3cd0a4aca
Gerald Donkor <donkorgerald8@gmail.com>
2025-10-04 14:58:34 +0000

    speed insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <SpeedInsights/>
       </body>
     </html>
   );
