fa55b6e9cc1d5eb7c686e7da6ac62e939d528c74
Gerald Donkor <donkorgerald8@gmail.com>
2025-10-04 12:23:01 +0000

    ignore eslint and typescript during builds

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-      ignoreDuringBuilds: true,
-  }, typescript: {
-      ignoreBuildErrors: true
+    eslint: {
+        ignoreDuringBuilds: true,
+    }, typescript: {
+        ignoreBuildErrors: true
     }
 };
 
